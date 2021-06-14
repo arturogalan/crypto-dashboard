c11f3389d2875573911669e2d6b71e645e1edba9
Arturo Galan <arturo.galan@gmail.com>
2021-06-14 02:51:35 +0200

    addGoogleFont

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <script>
   import BaseHeader from './components/BaseHeader.vue'
+  import BaseFooter from './components/BaseFooter.vue'
+
   export default {
     components: {
       BaseHeader,
+      BaseFooter,
     },
   }
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 <template>
+<div class="flex flex-col h-screen">
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <base-header/>
-  <router-view/>
+  <main class="flex-1 overflow-y-auto p-5">
+    <router-view/>
+    <base-footer/>
+  </main>
+</div>
 </template>
 
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Nunito, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;

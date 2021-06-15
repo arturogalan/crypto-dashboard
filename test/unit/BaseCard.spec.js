acd9b193b0621c098556f5fcd532bc3b4bbcd03e
Arturo Galan <arturo.galan@gmail.com>
2021-06-15 16:16:20 +0200

    jestTestTry

--- test/unit/BaseCard.spec.js
+++ test/unit/BaseCard.spec.js
@@ -1,9 +1,12 @@
-// src/components/HelloWorld.spec.ts
 import { mount } from '@vue/test-utils'
 import BaseCard from '../../src/components/BaseCard.vue'
+const MOCKS = {
+  $t: (key)=> key,
+  $n: (number)=> number,
+}
 
-describe('BaseCard', () => {
-  it('should display header text', () => {
+describe('BaseCard.vue component', () => {
+  it('it renders a prop and its content', () => {
     const wrapper = mount(BaseCard, {
       props: { 
         cardProperties: [
@@ -16,14 +19,52 @@ describe('BaseCard', () => {
         cardContent: {prop1: 'prop-content'},
       },
       global: {
-        mocks: {
-          $t: (key)=> key,
-          $n: (number)=> number,
-        }
+        mocks: MOCKS,
       }      
     })
     expect(wrapper.text()).toContain('prop-title')
     expect(wrapper.text()).toContain('prop-content')
-
+  })
+  it('if a prop doesnt exist then it renders N/A as value content', () => {
+    const wrapper = mount(BaseCard, {
+      props: { 
+        cardProperties: [
+          {
+            id: 'non-existent',
+            title: 'prop-non-existent-title',
+            type: 'string',
+          }
+        ],
+        cardContent: {prop1: 'prop-content'},
+      },
+      global: {
+        mocks: MOCKS,
+      }      
+    })
+    expect(wrapper.text()).toContain('prop-non-existent-title')
+    expect(wrapper.text()).toContain('N/A')
+  })  
+  it('it renders a title and a subtitle passed by slot', () => {
+    const wrapper = mount(BaseCard, {
+      props: { 
+        cardProperties: [
+          {
+            id: 'prop1',
+            title: 'prop-title',
+            type: 'string',
+          }
+        ],
+        cardContent: {prop1: 'prop-content'},
+      },
+      slots: {
+        title: 'This is the title',
+        subtitle: 'This is the subtitle',
+      },
+      global: {
+        mocks: MOCKS,
+      }      
+    })
+    expect(wrapper.text()).toContain('This is the title')
+    expect(wrapper.text()).toContain('This is the subtitle')    
   })
 })
